fa4c96ed738dc2b87b523442425a9cf62449d25b
baekkayoung <baekkayoung@naver.com>
2025-10-18 23:22:52 +0900

    10/18

--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 
 const Diary = () => {
   const params = useParams();
+  // useParams() URL에 포함된 파라미터를 객체로 꺼내주는 훅
   console.log(params); // diary/1 하면 id가 1로 뜸
   return <div>{params.id}번 일기입니다.</div>;
 };
